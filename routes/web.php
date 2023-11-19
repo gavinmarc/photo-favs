922cd87b6ed710b2d1706149de37943303eee694
Gavin Auerswald <g.auerswald@live.de>
2023-11-19 13:22:31 +0100

    Updated layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StartpageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', StartpageController::class)
+  ->name('start-page');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth'])
+  ->name('dashboard');
 
 require __DIR__.'/auth.php';
